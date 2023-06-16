5b1a8f0d4a0ece6ebcab06a0c3d305c2413e84fa
Victor Henrique <victor.henrique5800@gmail.com>
2023-06-15 22:20:00 -0300

    Implementing

--- adaboost_bindings/src/adaboost.rs
+++ adaboost_bindings/src/adaboost.rs
@@ -1,10 +1,14 @@
 use std::vec::Vec;
+use numpy::ndarray::{Array2, Axis};
+use numpy::{PyArray2, PyReadonlyArray1, PyReadonlyArray2};
+use crate::sample::Sample;
 use crate::weak_learner::WeakLearner;
 use crate::weighted_data::WeightedData;
 use pyo3::prelude::*;
 
 #[pyclass]
 pub struct AdaBoost {
+    pub n_estimators: usize,
     pub weak_learners: Vec<WeakLearner>,
     pub alphas: Vec<f64>,
 }
@@ -12,17 +16,38 @@ pub struct AdaBoost {
 #[pymethods]
 impl AdaBoost {
     #[new]
-    pub fn new() -> AdaBoost {
+    pub fn new(n_estimators: usize) -> AdaBoost {
         AdaBoost {
+            n_estimators,
             weak_learners: Vec::new(),
             alphas: Vec::new(),
         }
     }
 
-    pub fn fit(&mut self, weighted_data: WeightedData, n_estimators: usize) {
-        let mut weighted_data = weighted_data;
+    fn extractSamples(&self, x: PyReadonlyArray2<f64>, y: PyReadonlyArray1<f64>) -> Vec<Sample> {
+        let x: Array2<f64> = x.as_array().to_owned();
+        let y: Vec<f64> = y.as_array().to_owned().to_vec();
+        let weight = 1.0 / y.len() as f64;
+        let mut samples: Vec<Sample> = Vec::new();
 
-        for _ in 0..n_estimators {
+        for (i, row) in x.axis_iter(Axis(0)).enumerate() {
+            let features: Vec<f64> = row.iter()
+            .map(|x_n| *x_n)
+            .collect();
+
+            let label: i32 = y[i] as i32;
+            let sample = Sample::new(features, label, weight);
+            samples.push(sample);
+        }
+
+        return samples;
+    }
+
+    pub fn fit(&mut self, py: Python, x: PyReadonlyArray2<f64>, y: PyReadonlyArray1<f64>) -> PyResult<()>{
+        let samples = self.extractSamples(x, y);
+        let mut weighted_data = WeightedData::new(samples);
+
+        for _ in 0..self.n_estimators {
             let mut weak_learner = WeakLearner::new();
             weak_learner.fit(weighted_data.clone());
             let error = weighted_data.computeWeightedErrorRate(weak_learner.clone());
@@ -31,9 +56,13 @@ impl AdaBoost {
             self.weak_learners.push(weak_learner);
             self.alphas.push(alpha);
         }
+
+        Ok(())
     }
 
-    pub fn predict(&self, features: Vec<f64>) -> i32 {
+    pub fn predict(&self, py: Python, x: PyReadonlyArray2<f64>) -> i32 { // TODO: Antes o argumento era features: Vec<f64>
+        let x: Array2<f64> = x.as_array().to_owned();
+        
         let mut class_scores = vec![0.0; 2];
         for (weak_learner, alpha) in self.weak_learners.iter().zip(self.alphas.iter()) {
             let predicted_label = weak_learner.predict(features.clone());
--- main.py
+++ main.py
@@ -1,3 +1,56 @@
-from adaboost_bindings import AdaBoost
+from adaboost_bindings import AdaBoost # type: ignore
+import numpy as np
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+import time
 
-adaboost = AdaBoost()
+data = np.loadtxt('mnist.txt', delimiter=',')
+X = data[:, 1:]
+y = data[:, 0]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+
+n_estimators_range = np.arange(1, 11)
+sklearn_runtimes = []
+rust_runtimes = []
+sklearn_accuracies = []
+rust_accuracies = []
+
+for n_estimators in n_estimators_range:
+    # Create and fit an AdaBoost classifier from scikit-learn
+    sklearn_clf = AdaBoostClassifier(n_estimators=n_estimators)
+    start_time = time.time()
+    sklearn_clf.fit(X_train, y_train)
+    sklearn_runtimes.append(time.time() - start_time)
+
+    # Create and fit an AdaBoost classifier from our Rust implementation
+    rust_clf = AdaBoost(n_estimators)
+    start_time = time.time()
+    rust_clf.fit(X_train, y_train)
+    rust_runtimes.append(time.time() - start_time)
+
+    # Make predictions on the test set with both classifiers
+    sklearn_y_pred = sklearn_clf.predict(X_test)
+    rust_y_pred = rust_clf.predict(X_test)
+
+    # Compute the accuracy of both classifiers
+    sklearn_accuracies.append(accuracy_score(y_test, sklearn_y_pred))
+    rust_accuracies.append(accuracy_score(y_test, rust_y_pred))
+
+# Plot the runtime comparison
+plt.plot(n_estimators_range, sklearn_runtimes, label="scikit-learn")
+plt.plot(n_estimators_range, rust_runtimes, label="Rust")
+plt.xlabel("Number of estimators")
+plt.ylabel("Runtime (s)")
+plt.legend()
+plt.show()
+
+# Plot the accuracy comparison
+plt.plot(n_estimators_range, sklearn_accuracies, label="scikit-learn")
+plt.plot(n_estimators_range, rust_accuracies, label="Rust")
+plt.xlabel("Number of estimators")
+plt.ylabel("Accuracy")
+plt.legend()
+plt.show()
